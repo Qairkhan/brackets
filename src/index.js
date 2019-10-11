b37643824f7f638d34d57c503d3afd1719be54bc
qairkhan <shyngys94@gmai.com>
2019-10-11 22:27:00 +0300

    add:solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  if (str.length%2) {
+    return false;
+  }
+    var temp = str.split('');
+  for ( var i=1;i<temp.length;i++) {
+    for (var j=0;j<bracketsConfig.length;j++){
+      if ((temp[i] === bracketsConfig[j][1]) && (temp[i-1] === bracketsConfig[j][0])) {
+        temp.splice(i-1,2);
+        i-=2;
+      }		
+    }
+  }
+    return (temp.length>0)?false:true;
+  }
